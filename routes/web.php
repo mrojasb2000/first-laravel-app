62fd2e7618fd7a96d0a4230e65fc814468d42322
Mauricio Rojas <mrojasb2000@gmail.com>
2024-07-08 16:46:09 -0400

    refactor: replace index method by invoque method

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/', HomeController::class);
 
 Route::get('/posts', [PostController::class, 'index']);
 Route::get('/posts/create', [PostController::class, 'create']);
