95afbc32bb7c33e08daf7e6c29f41f526a30beae
Mauricio Rojas <mrojasb2000@gmail.com>
2024-07-09 18:37:21 -0400

    refactor: adding class component

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,4 +1,4 @@
-@props(['type'])
+@props(['type' => 'info'])
 
 @php
     switch ($type) {
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,10 +10,10 @@
 <body>
     <div class="max-w-4xl mx-auto px-4">
         <h1>Welcome to home page</h1>
-        <x-alert type="dark">
+        <x-alert2 type="warning">
             <x-slot name="title">Info </x-slot>
             Change a few things up and try submitting again.
-        </x-alert>
+        </x-alert2>
     </div>
 </body>
 </html>
