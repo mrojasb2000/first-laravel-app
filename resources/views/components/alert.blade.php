90526892bfb6d23732f0e83656456f64c67570b3
Mauricio Rojas <mrojasb2000@gmail.com>
2024-07-08 22:53:53 -0400

    refactor: change class definition alert

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -3,22 +3,22 @@
 @php
     switch ($type) {
         case 'danger':
-            $class = 'text-red-800 bg-red-50 dark:bg-gray-800 dark:text-red-400';
+            $class = 'text-red-800 rounded-lg bg-red-50 bg-gray-800 dark:text-red-400';
             break;
         case 'success':
-            $class = 'text-green-800 bg-green-50 dark:bg-gray-800 dark:text-green-400';
+            $class = 'text-green-800 rounded-lg bg-green-50 bg-gray-800 dark:text-green-400';
             break;    
         case 'warning':
-            $class = 'text-yellow-800 bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300';
+            $class = 'text-yellow-800 rounded-lg bg-yellow-50 bg-gray-800 dark:text-yellow-400';
             break;
         case 'dark':
-            $class = 'text-gray-800 bg-gray-50 dark:bg-gray-800 dark:text-gray-300';
+            $class = 'text-gray-800 rounded-lg bg-gray-50 bg-gray-800 dark:text-gray-300';
             break;    
         default:
-            $class = 'text-blue-800 bg-blue-50 dark:bg-gray-800 dark:text-blue-400';
+            $class = 'text-blue-800 rounded-lg bg-blue-50 bg-gray-700 dark:text-blue-400';
             break;
     }
 @endphp
-<div class="p-4 mb-4 text-sm rounded-lg {{$class}}" role="alert">
+<div class="p-4 mb-4 text-sm {{$class}}" role="alert">
     <span class="font-medium">{{ $title }}</span> {{ $slot }}
 </div>
