025bd6cdcc8e7e73da16c09ce6581efcb7137bed
Mauricio Rojas <mrojasb2000@gmail.com>
2024-07-09 22:39:19 -0400

    refactor: adding basic template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+{{-- <x-app-layout>
     <div class="max-w-4xl mx-auto px-4">
         <h1>Welcome to home page</h1>
         <x-alert2 type="warning">
@@ -6,4 +6,26 @@
             Change a few things up and try submitting again.
         </x-alert2>
     </div>
-</x-app-layout>
+</x-app-layout> --}}
+
+@extends('layouts.app')
+
+@section('title', 'Laravel 12')
+
+@push('css')
+    <style>
+        body {
+            background-color: #f3f3f3
+        }
+    </style>
+@endpush
+
+@section('content')
+<div class="max-w-4xl mx-auto px-4">
+    <h1>Welcome to home page</h1>
+    <x-alert2 type="warning">
+        <x-slot name="title">Info </x-slot>
+        Change a few things up and try submitting again.
+    </x-alert2>
+</div>
+@endsection
